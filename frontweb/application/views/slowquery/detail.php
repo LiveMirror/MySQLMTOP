de3b82c3157a0cffe3a89c25eac4f0e8f8f623e1
Ruzuojun <ruzuojun@139.com>
2014-01-26 12:37:06 +0800

    慢查询优化

--- frontweb/application/views/slowquery/detail.php
+++ frontweb/application/views/slowquery/detail.php
@@ -8,57 +8,98 @@
 
 <table class="table  table-bordered table-condensed" style="font-size: 12px;" >
     <tr>
-        <th style="width: 150px;">checksum</th>
-        <th colspan="4"><?php echo $record['checksum']; ?></th>
-        <th>sample</th>	
-        <th>sample</th>
+        <th style="width: 120px;">Reviewed</th>
+        <td colspan="6">Reviewed by DBA Ruzuojun at 2014-12-13 00:00:00</td>
+	</tr>
+    <tr>
+        <th style="width: 120px;">checksum</th>
+        <td colspan="2"><?php echo $record['checksum']; ?></td>
+        <th>ts_cnt</th>	
+        <td colspan="3"><?php echo $record['ts_cnt']; ?></td>
+	</tr>
+    <tr>
+        <th>first_seen</th>
+        <td colspan="2"><?php echo $record['first_seen']; ?></td>
+        <th>last_seen</th>
+        <td colspan="3"><?php echo $record['last_seen']; ?></td>
 	</tr>
     <tr>
         <th>fingerprint</th>
-        <th colspan="6"><?php echo $record['fingerprint']; ?></th>	
+        <td colspan="6"><?php echo $record['fingerprint']; ?></td>	
 	</tr>
     <tr>
         <th>sample</th>
-        <th colspan="6"><?php echo $record['sample']; ?></th>
+        <td colspan="6"><?php echo $record['sample']; ?></td>
 	</tr>
     <tr>
-        <th>first_seen</th>
-        <th><?php echo $record['first_seen']; ?></th>
-        <th>sample</th>
-        <th>sample</th>
-        <th>sample</th>	
-        <th>sample</th>
-        <th>sample</th>	
+        <th rowspan="2">Query Time</th>
+        <th>Query_time_sum</th>
+        <th>Query_time_min</th>
+        <th>Query_time_max</th>
+        <th>Query_time_pct_95</th>
+        <th>Query_time_stddev</th>
+        <th>Query_time_median</th>
 	</tr>
     <tr>
-        <th>last_seen</th>
-        <th><?php echo $record['last_seen']; ?></th>	
+        <td><?php echo $record['Query_time_sum']; ?></td>
+        <td><?php echo $record['Query_time_min']; ?></td>
+        <td><?php echo $record['Query_time_max']; ?></td>
+        <td><?php echo $record['Query_time_pct_95']; ?></td>
+        <td><?php echo $record['Query_time_stddev']; ?></td>
+        <td><?php echo $record['Query_time_median']; ?></td>
 	</tr>
     <tr>
-        <th>first_seen</th>
-        <th><?php echo $record['first_seen']; ?></th>	
+        <th rowspan="2">Lock Time</th>
+        <th>Lock_time_sum</th>
+        <th>Lock_time_min</th>
+        <th>Lock_time_max</th>
+        <th>Lock_time_pct_95</th>
+        <th>Lock_time_stddev</th>
+        <th>Lock_time_median</th>
 	</tr>
     <tr>
-        <th>last_seen</th>
-        <th><?php echo $record['last_seen']; ?></th>	
+        <td><?php echo $record['Lock_time_sum']; ?></td>
+        <td><?php echo $record['Lock_time_min']; ?></td>
+        <td><?php echo $record['Lock_time_max']; ?></td>
+        <td><?php echo $record['Lock_time_pct_95']; ?></td>
+        <td><?php echo $record['Lock_time_stddev']; ?></td>
+        <td><?php echo $record['Lock_time_median']; ?></td>
 	</tr>
     <tr>
-        <th>first_seen</th>
-        <th><?php echo $record['first_seen']; ?></th>	
+        <th rowspan="2">Rows Sent</th>
+        <th>Rows_sent_sum</th>
+        <th>Rows_sent_min</th>
+        <th>Rows_sent_max</th>
+        <th>Rows_sent_pct_95</th>
+        <th>Rows_sent_stddev</th>
+        <th>Rows_sent_median</th>
 	</tr>
     <tr>
-        <th>last_seen</th>
-        <th><?php echo $record['last_seen']; ?></th>	
+        <td><?php echo $record['Rows_sent_sum']; ?></td>
+        <td><?php echo $record['Rows_sent_min']; ?></td>
+        <td><?php echo $record['Rows_sent_max']; ?></td>
+        <td><?php echo $record['Rows_sent_pct_95']; ?></td>
+        <td><?php echo $record['Rows_sent_stddev']; ?></td>
+        <td><?php echo $record['Rows_sent_median']; ?></td>
 	</tr>
     <tr>
-        <th>first_seen</th>
-        <th><?php echo $record['first_seen']; ?></th>	
+        <th rowspan="2">Rows Examined</th>
+        <th>Rows_examined_sum</th>
+        <th>Rows_examined_min</th>
+        <th>Rows_examined_max</th>
+        <th>Rows_examined_pct_95</th>
+        <th>Rows_examined_stddev</th>
+        <th>Rows_examined_median</th>
 	</tr>
     <tr>
-        <th>last_seen</th>
-        <th><?php echo $record['last_seen']; ?></th>	
-	</tr>	
+        <td><?php echo $record['Rows_examined_sum']; ?></td>
+        <td><?php echo $record['Rows_examined_min']; ?></td>
+        <td><?php echo $record['Rows_examined_max']; ?></td>
+        <td><?php echo $record['Rows_examined_pct_95']; ?></td>
+        <td><?php echo $record['Rows_examined_stddev']; ?></td>
+        <td><?php echo $record['Rows_examined_median']; ?></td>
+	</tr>
 	 
 </table>
 
-
+<pre>备注:</pre>
